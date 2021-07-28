2e5b853e162d331caf679070176882cf6e3a99a2
David Hellebrand <david.hellebrand@elvac.eu>
2021-07-28 22:46:59 +0200

    Skript na znovu nahození trigerů

--- upravitdb2.php
+++ upravitdb2.php
@@ -1,13 +1,14 @@
 <?php
 define ("SQL_HOST","localhost");
 define ("SQL_USERNAME","root");
-define ("SQL_PASSWORD","");
+define ("SQL_PASSWORD","root");
 //$dbs = array("lmr2008","lmr2009","obrobna2008", "obrobna2009", "test2008","test2009");
-$dbs = array("lmr2011","obrobna2011","test2011");
+//$dbs = array("lmr2011","obrobna2011","test2011");
+$dbs = array("lmr2021");
 
 
 foreach ($dbs as $db) {
-    echo "databáze $db: ";
+    echo "databaze $db: ";
     $SRBD = mysqli_connect(SQL_HOST, SQL_USERNAME, SQL_PASSWORD) or Die(mysqli_error($SRBD));
     $vysledek = mysqli_select_db($SRBD, $db);
     if($vysledek == 0)   { //nepovedlo se pripojeni k DB
@@ -21,15 +22,16 @@ foreach ($dbs as $db) {
           mysqli_query($SRBD, 'delimiter //');
 
           $f = fopen("sql/transakce_triggery.sql", "r");
-          //$f = fopen("sql/drop_trigger.sql", "r");
+          $query = "";
           while (!feof ($f)) {
             $query .= fgets($f, 4096);
           }
           fclose ($f);
-          
 
           foreach (explode('//', $query) as $sql) {
-            mysqli_query($SRBD, $sql);
+            if($sql != "") {
+              mysqli_query($SRBD, $sql);
+            }
           }
 
           mysqli_query($SRBD, 'delimiter ;');
@@ -48,4 +50,4 @@ foreach ($dbs as $db) {
 
 ?>
 <br /><br />
-<a href="index.php">Zpìt na sklad</a>
+<a href="index.php">Zpet na sklad</a>
